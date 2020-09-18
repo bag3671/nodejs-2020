4c1ee3abc10780ca24facef96432eefc6eb9edad
sugyun baek <bag3671@hanmail.net>
2020-09-18 16:59:42 +0900

    basic

--- 01.helloworld.js
+++ 01.helloworld.js
@@ -10,4 +10,6 @@ console.log('1에서 100까지 정수의 합은 '+sum+'입니다.');
 let msg=`1에서 100까지 정수의 합은
 ${sum}
 입니다.`;
-console.log(msg);
+let D = new Date
+console.log(`올해는 ${D().getFullYear()}년 입니다.`)
+console.log(`올해는 ${D().getFullyear()}년 입니다.`)
---
+++ 02.basic.js
@@ -0,0 +1,63 @@
+// 원 둘레와 면적
+// let pi = 3.141592;
+// let radious = 5;
+
+// console.log(`반지름이 ${radious}인 원의 둘레는 ${2*pi*radious}입니다.`);
+// console.log(`반지름이 ${radious}인 원의 넓이는 ${pi*radious*radious}입니다.`);
+
+// 문자열 복합대입연산자
+// let output = "hello ";
+// output += "world ";
+// output +="! ";
+// output +='숫자도 가능합니다. ';
+// output += 235;
+
+// console.log(output);
+// let a = 10; b = 10; x = a++; y = ++b;
+// console.log(a, b, x, y);
+
+// 문자열
+// console.log(typeof('string'));
+// 숫자
+// console.log(typeof('271')) ;
+// 불
+// console.log(typeof(true));
+// 함수
+// console.log(typeof(funthion));
+// 객체
+// console.log(typeof({}));
+// undefined
+// console.log(typeof(alpha));
+
+// 자료형 변환
+// console.log(Number('273'));
+// console.log(Number(true));
+// console.log(Number('52.7'));
+
+// console.log(parseInt('273'));
+// console.log(parseInt(true));
+// console.log(parseInt('52.7'));
+
+// console.log(parseFloat('273'));
+// console.log(parseFloat(true));
+// console.log(parseFloat('52.7'));
+
+// 숫자와 문자열 자동 자료형 변환
+// console.log(52+273);
+// console.log("52"+273);
+// console.log(52+"273");
+// console.log("52"+"273");
+
+// 일치 연산자
+/* console.log(`52 == "52":${52 == "52"}`);
+console.log(`52 === "52": ${52 === "52"}`);
+console.log();
+console.log(`0 == "": ${0 == ""}`);
+console.log(`0 === "": ${0 === ""}`); */
+
+// 상수
+const constant = "변경할 수 없어요";
+// constant = "";
+console.log(constant);
+
+
